18960649f1594d8a0f5c4efb5fcde5a9f12e76d2
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-08-27 09:03:53 -0600

    finished challenge 4

--- forLoops/forLoops/forLoops.java
+++ forLoops/forLoops/forLoops.java
@@ -8,23 +8,36 @@ public class forLoops
 			//	showGeniusOne();
 				System.out.println();
 			//	showGeniusTwo();
-				showGeniusThree();
+			//	showGeniusThree();
+				showGeniusFour();
 
 			}
 		public static void showGeniusOne()
-		{
+			{
 			for (int i = 1; i < 6; i++)
 				System.out.println(i);
-		}
+			}
 		public static void showGeniusTwo()
-		{
+			{
 			for (int i = 2; i < 11; i=i + 2) 
 				{
 					System.out.println("I love Mullen");
 				}
-		}
+			}
 		public static void showGeniusThree()
-		{
-			
-		}
+			{
+			for (int i = 1; i < 6; i++) 
+				{ 
+					System.out.print(i);
+				}
+			}
+		public static void showGeniusFour();
+			{
+			for (int i = 10; i > 0; i--)
+				{
+					System.out.println(i);
+				}
+			System.out.println("");
+				
+			}
 	}
