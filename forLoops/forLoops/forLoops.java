dd829c9393614cc8a00eb0382d7076cc07a69451
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-08-27 08:56:41 -0600

    Just started third challenge

--- forLoops/forLoops/forLoops.java
+++ forLoops/forLoops/forLoops.java
@@ -5,13 +5,26 @@ public class forLoops
 
 		public static void main(String[] args)
 			{
-				showGeniusOne();
-				
-				//challengeTwo();
+			//	showGeniusOne();
+				System.out.println();
+			//	showGeniusTwo();
+				showGeniusThree();
 
 			}
-		public static void showGeniusOne() {
+		public static void showGeniusOne()
+		{
 			for (int i = 1; i < 6; i++)
 				System.out.println(i);
+		}
+		public static void showGeniusTwo()
+		{
+			for (int i = 2; i < 11; i=i + 2) 
+				{
+					System.out.println("I love Mullen");
+				}
+		}
+		public static void showGeniusThree()
+		{
+			
 		}
 	}
